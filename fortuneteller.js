120ebc075cf74f0d93c6b3c110c5b8fa05f65d24
Gerardo I Contreras <gcontreras@r2hstudent.org>
2022-06-25 16:10:59 -0500

    Added files, editted code (not finished)

---
+++ fortuneteller.js
@@ -0,0 +1,6 @@
+var numChildren = 5;
+var partnersName = "Jessica";
+var geoLoc = "Austin, Tx";
+var jobTitle = "WebDev";
+
+console.log("You will be a " + jobTitle + " in " + geoLoc + ", and married to " + partnersName + "with" );
